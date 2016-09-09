b11aa5dd80494b29acb7652f95b5fec42bdc88e5
Bobby Jap <RWJap85@gmail.com>
2016-09-09 00:03:08 -0700

    Updated gitignore to not include certain local Xcode files Removed the extra space from the folder name Initialized a new project space to work in for a C project for this crypto challenge All commits after this point should be on branches and code reviewed

---
+++ HandyCipher/HandyCipher/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  HandyCipher
+//
+//  Created by Bobby Jap on 9/8/16.
+//  Copyright © 2016 Bobby Jap, Ravee Khandagale. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
