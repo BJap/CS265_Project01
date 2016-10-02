63cf656cfcd1849e91da3024fdee5e5a4293ee6b
Bobby Jap <RWJap85@gmail.com>
2016-10-02 01:46:54 -0700

    Added more commenting Renamed variables to be better descriptions of themselves Abstracted some parts of the algorithm to encrypt

--- HandyCipher/HandyCipher/HandyCipher.c
+++ HandyCipher/HandyCipher/HandyCipher.c
@@ -11,22 +11,23 @@
 #include "HandyCipher.h"
 #include "HCKeyGen.h"
 
-static char keyTable[5][5];
-static char padTable[15];
-static int substitution[128];
-static char cEncrypt[6];
-static char eSelect[6];
-static char ePermute[6];
-static char eLast[6];
+static char keyTable[5][5];     // key table
+static char padTable[15];       // padding table
+static int  charMap[128];       // character to number mapping
+static char tuple[6];           // the row, column, or diagonal randomly selected
+static char subTuple[6];        // the characters matching with '1' bits of the number for the character mapping
+static char mixTuple[6];        // the random arragement of the selected characters in eSelect
+static char lastTuple[6];       // the last character substitution string created that meets all requirements
 
 #define T keyTable
-#define e cEncrypt
+#define e tuple
 #define random() rand()
 
-int pCount[5] = { 1, 2, 6, 24, 120 };
+// The amount of permututations (n!) for characters in subTuple
+int permCount[5] = { 1, 2, 6, 24, 120 };
 
 // Permutation Tables
-int perm [120][5] =
+int permTables [120][5] =
 {
     { 0, 1, 2, 3, 4 },
     { 1, 0, 2, 3, 4 },
@@ -172,35 +173,71 @@ int perm [120][5] =
 // Generate the two tables for encryption/decryption, and padding
 static void generateTables(char *key)
 {
-    int kp = 0;
-    int pp = 0;
+    int keyPos = 0;
+    int padPos = 0;
     
     for (int i = 0; i < 5; i++)
     {
         for (int j = 0; j < 8; j++)
         {
-            char c = key[kp++];
+            char c = key[keyPos++];
             
-            if (c == '^') c = key[kp++];
+            if (c == '^') c = key[keyPos++];
             
             if (j < 5) keyTable[i][j] = c;
-            else padTable[pp++] = c;
+            else padTable[padPos++] = c;
         }
     }
 }
 
 // Check to see if the encoding has only one bit in it
-static bool isSingleton(int sub)
+static bool isSingleton(int charValue)
 {
-    return (sub == 1) || (sub == 2) || (sub == 4) || (sub == 8) || (sub == 16);
+    return (charValue == 1) || (charValue == 2) || (charValue == 4) || (charValue == 8) || (charValue == 16);
+}
+
+// Assign the tuple to the character tuple array
+static void createTuple(int randTup)
+{
+    switch (randTup)
+    {
+        // Columns
+        case  0: e[0] = T[0][0]; e[1] = T[1][0]; e[2] = T[2][0]; e[3] = T[3][0]; e[4] = T[4][0]; break;
+        case  1: e[0] = T[0][1]; e[1] = T[1][1]; e[2] = T[2][1]; e[3] = T[3][1]; e[4] = T[4][1]; break;
+        case  2: e[0] = T[0][2]; e[1] = T[1][2]; e[2] = T[2][2]; e[3] = T[3][2]; e[4] = T[4][2]; break;
+        case  3: e[0] = T[0][3]; e[1] = T[1][3]; e[2] = T[2][3]; e[3] = T[3][3]; e[4] = T[4][3]; break;
+        case  4: e[0] = T[0][4]; e[1] = T[1][4]; e[2] = T[2][4]; e[3] = T[3][4]; e[4] = T[4][4]; break;
+            
+        // Rows
+        case  5: e[0] = T[0][0]; e[1] = T[0][1]; e[2] = T[0][2]; e[3] = T[0][3]; e[4] = T[0][4]; break;
+        case  6: e[0] = T[1][0]; e[1] = T[1][1]; e[2] = T[1][2]; e[3] = T[1][3]; e[4] = T[1][4]; break;
+        case  7: e[0] = T[2][0]; e[1] = T[2][1]; e[2] = T[2][2]; e[3] = T[2][3]; e[4] = T[2][4]; break;
+        case  8: e[0] = T[3][0]; e[1] = T[3][1]; e[2] = T[3][2]; e[3] = T[3][3]; e[4] = T[3][4]; break;
+        case  9: e[0] = T[4][0]; e[1] = T[4][1]; e[2] = T[4][2]; e[3] = T[4][3]; e[4] = T[4][4]; break;
+            
+        // Right Diagonal
+        case 10: e[0] = T[0][0]; e[1] = T[1][1]; e[2] = T[2][2]; e[3] = T[3][3]; e[4] = T[4][4]; break;
+        case 11: e[0] = T[0][1]; e[1] = T[1][2]; e[2] = T[2][3]; e[3] = T[3][4]; e[4] = T[4][0]; break;
+        case 12: e[0] = T[0][2]; e[1] = T[1][3]; e[2] = T[2][4]; e[3] = T[3][0]; e[4] = T[4][1]; break;
+        case 13: e[0] = T[0][3]; e[1] = T[1][4]; e[2] = T[2][0]; e[3] = T[3][1]; e[4] = T[4][2]; break;
+        case 14: e[0] = T[0][4]; e[1] = T[1][0]; e[2] = T[2][1]; e[3] = T[3][2]; e[4] = T[4][3]; break;
+            
+        // Left Diagonal
+        case 15: e[0] = T[0][0]; e[1] = T[1][4]; e[2] = T[2][3]; e[3] = T[3][2]; e[4] = T[4][1]; break;
+        case 16: e[0] = T[0][1]; e[1] = T[1][0]; e[2] = T[2][4]; e[3] = T[3][3]; e[4] = T[4][2]; break;
+        case 17: e[0] = T[0][2]; e[1] = T[1][1]; e[2] = T[2][0]; e[3] = T[3][4]; e[4] = T[4][3]; break;
+        case 18: e[0] = T[0][3]; e[1] = T[1][2]; e[2] = T[2][1]; e[3] = T[3][0]; e[4] = T[4][4]; break;
+        case 19: e[0] = T[0][4]; e[1] = T[1][3]; e[2] = T[2][2]; e[3] = T[3][1]; e[4] = T[4][0]; break;
+    }
 }
 
 // Check to see if the choice of encryption will allow unique reading on decryption
-static bool validateLine(int rTup, char next, bool singletonLast)
+static bool validateLine(int randTup, char next, bool singletonLast)
 {
-    if (strchr(eLast, ePermute[0]) != NULL) return false;
+    // the new line is invalid if the first character was in the last line added to the cipher
+    if (strchr(lastTuple, mixTuple[0]) != NULL) return false;
     
-    if (rTup >= 5 && rTup <= 9 && isSingleton(substitution[next])) return false;
+    if (randTup >= 5 && randTup <= 9 && isSingleton(charMap[next])) return false;
     
     if (singletonLast)
     {
@@ -208,17 +245,17 @@ static bool validateLine(int rTup, char next, bool singletonLast)
         {
             for (int j = 0; j < 5; j++)
             {
-                if (T[i][j] == ePermute[0])
+                if (T[i][j] == mixTuple[0])
                 {
                     for (int k = 0; k < 5; k++)
                     {
-                        if (T[i][k] == eLast[0]) return false;
+                        if (T[i][k] == lastTuple[0]) return false;
                         
-                        if (T[k][j] == eLast[0]) return false;
+                        if (T[k][j] == lastTuple[0]) return false;
                         
-                        if (T[i + k < 5 ? i + k : i + k - 5][j + k < 5 ? j + k : j + k - 5] == eLast[0]) return false;
+                        if (T[i + k < 5 ? i + k : i + k - 5][j + k < 5 ? j + k : j + k - 5] == lastTuple[0]) return false;
                         
-                        if (T[i + k < 5 ? i + k : i + k - 5][j - k >= 0 ? j - k : j - k + 5] == eLast[0]) return false;
+                        if (T[i + k < 5 ? i + k : i + k - 5][j - k >= 0 ? j - k : j - k + 5] == lastTuple[0]) return false;
                     }
                 }
             }
@@ -226,16 +263,6 @@ static bool validateLine(int rTup, char next, bool singletonLast)
     }
     
     return true;
-}
-
-// TODO
-static char *tuple(int letterNum)
-{
-    char *tuple;
-    
-    // TODO
-    
-    return tuple;
 }
 
 char *encryptText(char *text, char *key)
@@ -248,84 +275,61 @@ char *encryptText(char *text, char *key)
     char *subKey = generateSubKey(key);
     
     generateTables(key);
-    generateSubstitution(subKey, substitution);
+    generateSubstitution(subKey, charMap);
     
-    size_t length = strlen(text);
+    size_t textLength = strlen(text);
     
-    int iCipher = 0;
+    int cipherPos = 0;
     
-    for (int i = 0; i < length; i++)
+    // find a line to substitute in for each character in the text
+    for (int i = 0; i < textLength; i++)
     {
         char c = text[i];
-        int sub = substitution[c];
+        int charValue = charMap[c];
         
-        if (sub == 0) continue;
+        if (charValue == 0) continue;
         
-        bool singleton = isSingleton(sub);
-        int rTup;
+        bool singleton = isSingleton(charValue);
+        int randTup;
         
+        // generate a line until one satisfies the restrictions for the cipher
         do
         {
-            rTup = random() % (singleton ? 5 : 20);
+            randTup = random() % (singleton ? 5 : 20);
             
-            switch (rTup)
-            {
-                // Columns
-                case  0: e[0] = T[0][0]; e[1] = T[1][0]; e[2] = T[2][0]; e[3] = T[3][0]; e[4] = T[4][0]; break;
-                case  1: e[0] = T[0][1]; e[1] = T[1][1]; e[2] = T[2][1]; e[3] = T[3][1]; e[4] = T[4][1]; break;
-                case  2: e[0] = T[0][2]; e[1] = T[1][2]; e[2] = T[2][2]; e[3] = T[3][2]; e[4] = T[4][2]; break;
-                case  3: e[0] = T[0][3]; e[1] = T[1][3]; e[2] = T[2][3]; e[3] = T[3][3]; e[4] = T[4][3]; break;
-                case  4: e[0] = T[0][4]; e[1] = T[1][4]; e[2] = T[2][4]; e[3] = T[3][4]; e[4] = T[4][4]; break;
-                    
-                // Rows
-                case  5: e[0] = T[0][0]; e[1] = T[0][1]; e[2] = T[0][2]; e[3] = T[0][3]; e[4] = T[0][4]; break;
-                case  6: e[0] = T[1][0]; e[1] = T[1][1]; e[2] = T[1][2]; e[3] = T[1][3]; e[4] = T[1][4]; break;
-                case  7: e[0] = T[2][0]; e[1] = T[2][1]; e[2] = T[2][2]; e[3] = T[2][3]; e[4] = T[2][4]; break;
-                case  8: e[0] = T[3][0]; e[1] = T[3][1]; e[2] = T[3][2]; e[3] = T[3][3]; e[4] = T[3][4]; break;
-                case  9: e[0] = T[4][0]; e[1] = T[4][1]; e[2] = T[4][2]; e[3] = T[4][3]; e[4] = T[4][4]; break;
-                    
-                // Right Diagonal
-                case 10: e[0] = T[0][0]; e[1] = T[1][1]; e[2] = T[2][2]; e[3] = T[3][3]; e[4] = T[4][4]; break;
-                case 11: e[0] = T[0][1]; e[1] = T[1][2]; e[2] = T[2][3]; e[3] = T[3][4]; e[4] = T[4][0]; break;
-                case 12: e[0] = T[0][2]; e[1] = T[1][3]; e[2] = T[2][4]; e[3] = T[3][0]; e[4] = T[4][1]; break;
-                case 13: e[0] = T[0][3]; e[1] = T[1][4]; e[2] = T[2][0]; e[3] = T[3][1]; e[4] = T[4][2]; break;
-                case 14: e[0] = T[0][4]; e[1] = T[1][0]; e[2] = T[2][1]; e[3] = T[3][2]; e[4] = T[4][3]; break;
-                    
-                // Left Diagonal
-                case 15: e[0] = T[0][0]; e[1] = T[1][4]; e[2] = T[2][3]; e[3] = T[3][2]; e[4] = T[4][1]; break;
-                case 16: e[0] = T[0][1]; e[1] = T[1][0]; e[2] = T[2][4]; e[3] = T[3][3]; e[4] = T[4][2]; break;
-                case 17: e[0] = T[0][2]; e[1] = T[1][1]; e[2] = T[2][0]; e[3] = T[3][4]; e[4] = T[4][3]; break;
-                case 18: e[0] = T[0][3]; e[1] = T[1][2]; e[2] = T[2][1]; e[3] = T[3][0]; e[4] = T[4][4]; break;
-                case 19: e[0] = T[0][4]; e[1] = T[1][3]; e[2] = T[2][2]; e[3] = T[3][1]; e[4] = T[4][0]; break;
-            }
+            createTuple(randTup);
             
-            bool odd = (i % 2) == 0;
-            int count = 0;
+            bool oddPos = (i % 2) == 0;
+            int subTupLen = 0;
             
+            // select the letters from the tuple that align with the '1's in the binary of the number
+            // for the given character and in the correct direction
             for (int j = 0, mask = 0x10; j < 5; j++, mask >>= 1)
             {
-                if (sub & mask) eSelect[count++] = odd ? e[j] : e[4 - j];
+                if (charValue & mask) subTuple[subTupLen++] = oddPos ? e[j] : e[4 - j];
             }
             
-            eSelect[count] = '\0';
+            subTuple[subTupLen] = '\0';
             
-            int rPerm = random() % pCount[count - 1];
+            int randPerm = random() % permCount[subTupLen - 1];
             
-            for (int j = 0; j < count; j++)
+            // reorder the line chosen for substitution
+            for (int j = 0; j < subTupLen; j++)
             {
-                ePermute[j] = eSelect[perm[rPerm][j]];
+                mixTuple[j] = subTuple[permTables[randPerm][j]];
             }
             
-            ePermute[count] = '\0';
+            mixTuple[subTupLen] = '\0';
             
-        } while (!validateLine(rTup, text[i + 1], singletonLast));
+        } while (!validateLine(randTup, text[i + 1], singletonLast));
         
-        for (int j = 0; ePermute[j] != '\0'; j++)
+        // concatenate the line onto the cipher text
+        for (int j = 0; mixTuple[j] != '\0'; j++)
         {
-            cipher[iCipher++] = ePermute[j];
+            cipher[cipherPos++] = mixTuple[j];
         }
         
-        memcpy(eLast, ePermute, 6);
+        memcpy(lastTuple, mixTuple, 6);
         
         singletonLast = singleton;
     }
